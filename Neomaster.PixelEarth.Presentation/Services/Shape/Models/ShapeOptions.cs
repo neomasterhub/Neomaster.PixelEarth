86dc9d7dd3496364b12de6515c93ccf35a51e683
Neomaster X <neomasterx@outlook.com>
2025-11-28 16:14:15 +0300

    #10 Show side flags, front side flag

--- Neomaster.PixelEarth.Presentation/Services/Shape/Models/ShapeOptions.cs
+++ Neomaster.PixelEarth.Presentation/Services/Shape/Models/ShapeOptions.cs
@@ -5,4 +5,7 @@ public record ShapeOptions
   public int ShaderProgramId { get; set; }
   public ShaderProgramArgs ShaderProgramArgs { get; set; }
   public DrawMode DrawMode { get; set; }
+  public DrawMode FrontSideDrawMode { get; set; }
+  public bool ShowFrontSide { get; set; }
+  public bool ShowBackSide { get; set; }
 }
