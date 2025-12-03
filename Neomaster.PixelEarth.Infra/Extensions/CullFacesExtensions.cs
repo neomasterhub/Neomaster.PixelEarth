6a2ad690f3c80c300b2cbb4f4635d78839b2399f
Neomaster X <neomasterx@outlook.com>
2025-12-03 16:33:09 +0300

    #12 CullFaces.Apply()

---
+++ Neomaster.PixelEarth.Infra/Extensions/CullFacesExtensions.cs
@@ -0,0 +1,29 @@
+using Neomaster.PixelEarth.Presentation;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Neomaster.PixelEarth.Infra.Extensions;
+
+public static class CullFacesExtensions
+{
+  public static void Apply(this CullFaces cullFaces)
+  {
+    switch (cullFaces)
+    {
+      case CullFaces.None:
+        break;
+      case CullFaces.Front:
+        GL.Enable(EnableCap.CullFace);
+        GL.CullFace(TriangleFace.Front);
+        break;
+      case CullFaces.Back:
+        GL.Enable(EnableCap.CullFace);
+        GL.CullFace(TriangleFace.Back);
+        break;
+      case CullFaces.FrontAndBack:
+        GL.Enable(EnableCap.CullFace);
+        GL.CullFace(TriangleFace.FrontAndBack);
+        break;
+      default: throw new IndexOutOfRangeException();
+    }
+  }
+}
