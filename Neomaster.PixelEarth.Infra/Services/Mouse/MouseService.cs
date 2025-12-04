5a6bc503e863ec3433258887dd16659a499c82dc
Neomaster X <neomasterx@outlook.com>
2025-12-04 20:45:57 +0300

    #23 MouseState.LeftPressed

--- Neomaster.PixelEarth.Infra/Services/Mouse/MouseService.cs
+++ Neomaster.PixelEarth.Infra/Services/Mouse/MouseService.cs
@@ -12,16 +12,20 @@ public class MouseService : IMouseService
     MouseState = e.MouseState;
   }
 
-  public bool IsInRectangle(Vector2 topLeft, Vector2 bottomRight)
+  public AreaMouseState GetRectangleMouseState(Vector2 topLeft, Vector2 bottomRight)
   {
     var xMin = MathF.Min(topLeft.X, bottomRight.X);
     var xMax = MathF.Max(topLeft.X, bottomRight.X);
     var yMin = MathF.Min(topLeft.Y, bottomRight.Y);
     var yMax = MathF.Max(topLeft.Y, bottomRight.Y);
 
-    return MouseState.Position.X >= xMin
+    var isIn = MouseState.Position.X >= xMin
       && MouseState.Position.X <= xMax
       && MouseState.Position.Y >= yMin
       && MouseState.Position.Y <= yMax;
+
+    return new AreaMouseState(
+      isIn,
+      MouseState.LeftPressed);
   }
 }
