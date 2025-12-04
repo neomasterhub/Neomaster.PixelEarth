ebf8f8dea21aec7b2ccf8ecd580df998a5225b57
Neomaster X <neomasterx@outlook.com>
2025-12-04 04:57:33 +0300

    #6 Shader fills: normal, hovered, selected

--- Neomaster.PixelEarth.Infra/Shaders/fragment.glsl
+++ Neomaster.PixelEarth.Infra/Shaders/fragment.glsl
@@ -1,9 +1,25 @@
 #version 330 core
 
 out vec4 FragColor;
-uniform vec4 uFill;
+uniform vec4 uFillNormal;
+uniform vec4 uFillHovered;
+uniform vec4 uFillSelected;
+uniform bool uIsHovered;
+uniform bool uIsSelected;
 
 void main()
 {
-  FragColor = uFill;
+  if (uIsSelected)
+  {
+    FragColor = uFillSelected;
+    return;
+  }
+
+  if (uIsHovered)
+  {
+    FragColor = uFillHovered;
+    return;
+  }
+
+  FragColor = uFillNormal;
 }
