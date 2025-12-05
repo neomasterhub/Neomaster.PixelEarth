0a5218316cd92055957170476cba8b8cf4387088
Neomaster X <neomasterx@outlook.com>
2025-12-05 06:14:23 +0300

    #7 DrawButton()

--- Neomaster.PixelEarth.Infra/Services/UI/UIService.cs
+++ Neomaster.PixelEarth.Infra/Services/UI/UIService.cs
@@ -4,9 +4,45 @@ using Neomaster.PixelEarth.Presentation;
 namespace Neomaster.PixelEarth.Infra;
 
 public class UIService(
-  IIdGenerator<int> idGenerator)
+  IIdGenerator<int> idGenerator,
+  IShapeService shapeService)
   : IUIService
 {
+  private int _selectedId;
+
+  public void DrawButton(
+    Button button,
+    ShapeOptions? shapeOptions = null)
+  {
+    shapeOptions ??= PresentationConsts.Shape.DefaultOptions;
+
+    var isSelected = button.Id == _selectedId;
+    var shapeFillNormal = isSelected
+      ? button.Options.FillNormal
+      : button.Options.FillSelected;
+    var shapeFillHovered = isSelected
+      ? button.Options.FillHovered
+      : button.Options.FillSelectedHovered;
+
+    shapeOptions = shapeOptions.Value
+      .FillNormal(shapeFillNormal)
+      .FillHovered(shapeFillHovered);
+
+    var shapeState = shapeService.DrawRectangle(
+      button.X,
+      button.Y,
+      button.Width,
+      button.Height,
+      shapeOptions);
+
+    if (shapeState.IsMouseLeftPressed)
+    {
+      _selectedId = shapeState.IsHovered
+        ? button.Id
+        : 0; // TODO: Problem with layers.
+    }
+  }
+
   public Button CreateButton(
     float x,
     float y,
