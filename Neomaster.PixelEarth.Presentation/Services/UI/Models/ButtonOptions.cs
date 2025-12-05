457b7df7714bd19ac12821b90566456ebf3ccdd7
Neomaster X <neomasterx@outlook.com>
2025-12-05 05:51:26 +0300

    #7 ButtonOptions

---
+++ Neomaster.PixelEarth.Presentation/Services/UI/Models/ButtonOptions.cs
@@ -0,0 +1,11 @@
+using System.Numerics;
+
+namespace Neomaster.PixelEarth.Presentation;
+
+public struct ButtonOptions
+{
+  public Vector4 FillNormal;
+  public Vector4 FillHovered;
+  public Vector4 FillSelected;
+  public Vector4 FillSelectedHovered;
+}
