6f34b101412f41af45892d8720f8270a3bcab8f8
Neomaster X <neomasterx@outlook.com>
2025-12-09 22:15:41 +0300

    #33 Grid align props

--- Neomaster.PixelEarth.Presentation/Services/UI/Models/Grid.cs
+++ Neomaster.PixelEarth.Presentation/Services/UI/Models/Grid.cs
@@ -10,5 +10,7 @@ public class Grid<TCell>(int id)
   public float CellWidth { get; set; }
   public float CellHeight { get; set; }
   public float Gap { get; set; }
+  public Align VerticalAlign { get; set; }
+  public Align HorizontalAlign { get; set; }
   public List<TCell> Cells { get; set; } = [];
 }
