c90ceef5ae3889669d4584b93ea30c4ac8d34051
Neomaster X <neomasterx@outlook.com>
2026-01-06 15:22:43 +0300

    #42 TextureService.Load()

---
+++ Neomaster.PixelEarth.Infra/Services/Texture/TextureService.cs
@@ -0,0 +1,11 @@
+using Neomaster.PixelEarth.Presentation;
+
+namespace Neomaster.PixelEarth.Infra;
+
+public class TextureService : ITextureService
+{
+  public TextureInfo Load(string fileName)
+  {
+    throw new NotImplementedException();
+  }
+}
