3da55c0a8cdf192c09fa39545c6ecccbf44ef066
Neomaster X <neomasterx@outlook.com>
2026-01-07 16:35:29 +0300

    #42 Load texture

--- Neomaster.PixelEarth.Infra/Services/Texture/TextureService.cs
+++ Neomaster.PixelEarth.Infra/Services/Texture/TextureService.cs
@@ -1,18 +1,34 @@
 using Neomaster.PixelEarth.Presentation;
-using SkiaSharp;
+using OpenTK.Graphics.OpenGL4;
 
 namespace Neomaster.PixelEarth.Infra;
 
-public class TextureService : ITextureService
+public class TextureService(
+  IImageService imageService)
+  : ITextureService
 {
   public TextureInfo Load(string fileName)
   {
-    var path = Path.Combine(InfraConsts.Dirs.Textures, fileName);
-    using var original = SKBitmap.Decode(path);
+    var imageInfo = imageService.GetImageInfo(fileName, flipY: true);
+
+    var textureId = GL.GenTexture();
+
+    GL.BindTexture(TextureTarget.Texture2D, textureId);
+    GL.TexImage2D(
+      target: TextureTarget.Texture2D,
+      level: 0,
+      PixelInternalFormat.Rgba,
+      imageInfo.Width,
+      imageInfo.Height,
+      border: 0,
+      PixelFormat.Rgba,
+      PixelType.UnsignedByte,
+      imageInfo.Bytes);
+    GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 
     return new TextureInfo
     {
-      Path = path,
+      Id = textureId,
     };
   }
 }
--- Neomaster.PixelEarth.Presentation/Services/Texture/Models/TextureInfo.cs
+++ Neomaster.PixelEarth.Presentation/Services/Texture/Models/TextureInfo.cs
@@ -3,5 +3,4 @@ namespace Neomaster.PixelEarth.Presentation;
 public record TextureInfo
 {
   public int Id { get; init; }
-  public int Path { get; init; }
 }
