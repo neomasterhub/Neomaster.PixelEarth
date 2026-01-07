424d76a65574a02b458713a11a550ff5e2599d97
Neomaster X <neomasterx@outlook.com>
2026-01-07 16:12:50 +0300

    #42 Flip loaded image

--- Neomaster.PixelEarth.Infra/Services/Image/ImageService.cs
+++ Neomaster.PixelEarth.Infra/Services/Image/ImageService.cs
@@ -5,18 +5,40 @@ namespace Neomaster.PixelEarth.Infra;
 
 public class ImageService : IImageService
 {
-  public ImageInfo GetImageInfo(string fileName)
+  public ImageInfo GetImageInfo(
+    string fileName,
+    bool flipX = false,
+    bool flipY = false)
   {
     var path = Path.Combine(InfraConsts.Dirs.Textures, fileName);
-    using var bitmap = SKBitmap.Decode(path);
+    using var bitmapSrc = SKBitmap.Decode(path);
+    var bitmapResult = bitmapSrc;
 
-    return new ImageInfo
+    if (flipX || flipY)
+    {
+      bitmapResult = new SKBitmap(bitmapSrc.Width, bitmapSrc.Height);
+      using var canvas = new SKCanvas(bitmapResult);
+
+      var scaleX = flipX ? -1f : 1f;
+      var scaleY = flipY ? -1f : 1f;
+      canvas.Scale(scaleX, scaleY);
+
+      var translateX = flipX ? -bitmapSrc.Width : 0;
+      var translateY = flipY ? -bitmapSrc.Height : 0;
+      canvas.Translate(translateX, translateY);
+
+      canvas.DrawBitmap(bitmapSrc, 0, 0);
+    }
+
+    var result = new ImageInfo
     {
       FilePath = path,
-      Width = bitmap.Width,
-      Height = bitmap.Height,
-      Size = bitmap.ByteCount,
-      Bytes = bitmap.Bytes,
+      Width = bitmapResult.Width,
+      Height = bitmapResult.Height,
+      Size = bitmapResult.ByteCount,
+      Bytes = bitmapResult.Bytes,
     };
+
+    return result;
   }
 }
--- Neomaster.PixelEarth.Presentation/Services/Image/IImageService.cs
+++ Neomaster.PixelEarth.Presentation/Services/Image/IImageService.cs
@@ -2,5 +2,8 @@ namespace Neomaster.PixelEarth.Presentation;
 
 public interface IImageService
 {
-  ImageInfo GetImageInfo(string fileName);
+  ImageInfo GetImageInfo(
+    string fileName,
+    bool flipX = false,
+    bool flipY = false);
 }
