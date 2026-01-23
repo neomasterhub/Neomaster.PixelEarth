03ee1a207ebdde416210118efb1e895de1d309f8
Neomaster X <neomasterx@outlook.com>
2026-01-23 10:41:00 +0300

    #56 TextureShapeOptions

--- Neomaster.PixelEarth.App/Services/Shape/Models/TextureShapeOptions.cs
+++ Neomaster.PixelEarth.App/Services/Shape/Models/TextureShapeOptions.cs
@@ -2,4 +2,26 @@ namespace Neomaster.PixelEarth.App;
 
 public struct TextureShapeOptions
 {
+  public int TextureId;
+  public int TextureIdHovered;
+  public int TextureIdSelected;
+  public int TextureIdSelectedHovered;
+  public bool IsHovered;
+  public bool IsSelected;
+
+  public readonly int CurrentTextureId => IsHovered
+    ? (IsSelected ? TextureIdSelectedHovered : TextureIdHovered)
+    : (IsSelected ? TextureIdSelected : TextureId);
+
+  public TextureShapeOptions SetHovered(bool isHovered)
+  {
+    IsHovered = isHovered;
+    return this;
+  }
+
+  public TextureShapeOptions SetSelected(bool isSelected)
+  {
+    IsSelected = isSelected;
+    return this;
+  }
 }
