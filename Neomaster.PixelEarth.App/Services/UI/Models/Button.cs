4935aecfe8223309b49a655690be5c1ab29f1191
Neomaster X <neomasterx@outlook.com>
2026-01-26 09:37:13 +0300

    #83 abstract class Button
    Related Work Items: #80

--- Neomaster.PixelEarth.App/Services/UI/Models/Button.cs
+++ Neomaster.PixelEarth.App/Services/UI/Models/Button.cs
@@ -1,11 +1,10 @@
 namespace Neomaster.PixelEarth.App;
 
-public class Button(int id)
+public abstract class Button(int id)
   : UIElement
 {
   public override int Id => id;
   public float Width { get; set; }
   public float Height { get; set; }
   public Action Action { get; set; }
-  public ButtonOptions Options { get; set; }
 }
