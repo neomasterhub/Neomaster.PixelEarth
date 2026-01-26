bad3fec5d638ecdd84f33d9b599417ae9adf7f82
Neomaster X <neomasterx@outlook.com>
2026-01-26 08:31:41 +0300

    #65 TextureButtonOptions

---
+++ Neomaster.PixelEarth.App/Services/UI/Models/TextureButtonOptions.cs
@@ -0,0 +1,9 @@
+namespace Neomaster.PixelEarth.App;
+
+public struct TextureButtonOptions
+{
+  public int TextureId;
+  public int TextureIdHovered;
+  public int TextureIdSelected;
+  public int TextureIdSelectedHovered;
+}
