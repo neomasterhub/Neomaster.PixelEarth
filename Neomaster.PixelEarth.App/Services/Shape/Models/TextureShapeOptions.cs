98dc7029cac35fdf83bd14619bf64e083f8f30c6
Neomaster X <neomasterx@outlook.com>
2026-01-27 04:42:00 +0300

    #89 TextureShapeOptions.TextureUv*XYWidthHeight

--- Neomaster.PixelEarth.App/Services/Shape/Models/TextureShapeOptions.cs
+++ Neomaster.PixelEarth.App/Services/Shape/Models/TextureShapeOptions.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace Neomaster.PixelEarth.App;
 
 public struct TextureShapeOptions
@@ -6,6 +8,10 @@ public struct TextureShapeOptions
   public int TextureHoveredId;
   public int TextureSelectedId;
   public int TextureSelectedHoveredId;
+  public Vector4 TextureUvXYWidthHeight;
+  public Vector4 TextureHoveredUvXYWidthHeight;
+  public Vector4 TextureSelectedUvXYWidthHeight;
+  public Vector4 TextureSelectedHoveredUvXYWidthHeight;
   public bool IsHovered;
   public bool IsSelected;
 
@@ -13,7 +19,11 @@ public struct TextureShapeOptions
     Texture normal,
     Texture hovered = null,
     Texture selected = null,
-    Texture selectedHovered = null)
+    Texture selectedHovered = null,
+    Vector4? textureUvXYWidthHeight = null,
+    Vector4? textureHoveredUvXYWidthHeight = null,
+    Vector4? textureSelectedUvXYWidthHeight = null,
+    Vector4? textureSelectedHoveredUvXYWidthHeight = null)
   {
     ArgumentNullException.ThrowIfNull(normal);
 
@@ -26,6 +36,17 @@ public struct TextureShapeOptions
     TextureHoveredId = hovered?.LoadedId ?? TextureId;
     TextureSelectedId = selected?.LoadedId ?? TextureId;
     TextureSelectedHoveredId = selectedHovered?.LoadedId ?? TextureId;
+
+    TextureUvXYWidthHeight = textureUvXYWidthHeight ?? new Vector4(0, 0, normal.Width, normal.Height);
+
+    TextureHoveredUvXYWidthHeight = textureHoveredUvXYWidthHeight
+      ?? (hovered == null ? TextureUvXYWidthHeight : new Vector4(0, 0, hovered.Width, hovered.Height));
+
+    TextureSelectedUvXYWidthHeight = textureSelectedUvXYWidthHeight
+      ?? (selected == null ? TextureUvXYWidthHeight : new Vector4(0, 0, selected.Width, selected.Height));
+
+    TextureSelectedHoveredUvXYWidthHeight = textureSelectedHoveredUvXYWidthHeight
+      ?? (selectedHovered == null ? TextureUvXYWidthHeight : new Vector4(0, 0, selectedHovered.Width, selectedHovered.Height));
   }
 
   public readonly int CurrentTextureId => IsHovered
