d093913355e5e641a58cdb8bdd95b696228177ba
Neomaster X <neomasterx@outlook.com>
2026-01-28 05:15:32 +0300

    #91 GetRectangleMouseState(x, y, width, height)

--- Neomaster.PixelEarth.App/Services/Mouse/IMouseService.cs
+++ Neomaster.PixelEarth.App/Services/Mouse/IMouseService.cs
@@ -7,4 +7,5 @@ public interface IMouseService
   MouseState MouseState { get; }
   void UpdateMouseState(MouseStateEventArgs e);
   AreaMouseState GetRectangleMouseState(Vector2 topLeft, Vector2 bottomRight);
+  AreaMouseState GetRectangleMouseState(float x, float y, float width, float height);
 }
