4d4cafe0374d17070e1f72c6efe4abb45bd35f82
Neomaster X <neomasterx@outlook.com>
2026-02-04 12:04:48 +0300

    #106 GameStage.Condition

--- Neomaster.PixelEarth.App/Services/GamePipeline/Models/GameStage.cs
+++ Neomaster.PixelEarth.App/Services/GamePipeline/Models/GameStage.cs
@@ -6,11 +6,14 @@ public class GameStage
 {
   public GameStage()
   {
+    Condition = () => false;
     OnWindowRender = e => { };
     OnWindowUpdate = e => { };
   }
 
   public int Id { get; set; }
+  public string Name { get; set; }
+  public Func<bool> Condition { get; set; }
   public Action<RenderEventArgs?> OnWindowRender { get; set; }
   public Action<UpdateEventArgs?> OnWindowUpdate { get; set; }
 }
