f421d4d25d738d00069c5882004a46fb1235565e
Neomaster X <neomasterx@outlook.com>
2026-02-08 20:15:34 +0300

    #119 Clear AppConsts

--- Neomaster.PixelEarth.App/AppConsts.cs
+++ Neomaster.PixelEarth.App/AppConsts.cs
@@ -2,19 +2,4 @@ namespace Neomaster.PixelEarth.App;
 
 public static class AppConsts
 {
-  public static class TextureName
-  {
-    public const string Test512x512 = "Test 512x512";
-    public const string Test512x512Hovered = "Test 512x512 Hovered";
-    public const string Test512x512Selected = "Test 512x512 Selected";
-    public const string Test512x512SelectedHovered = "Test 512x512 Selected Hovered";
-    public const string Ground = "Ground";
-  }
-
-  public static class TextureGroupName
-  {
-    public const string Test = "Test";
-    public const string MainMenu = "Main Menu";
-    public const string Level1 = "Level 1";
-  }
 }
