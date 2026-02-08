43948333063805373800c06eec6175d90014fc6d
Neomaster X <neomasterx@outlook.com>
2026-02-08 11:17:37 +0300

    #109 fix: GameStateFlag: starts from 1

--- Neomaster.PixelEarth.Presentation/Models/GameStateFlag.cs
+++ Neomaster.PixelEarth.Presentation/Models/GameStateFlag.cs
@@ -3,7 +3,7 @@ namespace Neomaster.PixelEarth.Presentation;
 [Flags]
 public enum GameStateFlag
 {
-  None = 0,
-  Loading = 1,
-  ShowMainMenu = 1 << 1,
+  None = 1,
+  Loading = 1 << 1,
+  ShowMainMenu = 1 << 2,
 }
