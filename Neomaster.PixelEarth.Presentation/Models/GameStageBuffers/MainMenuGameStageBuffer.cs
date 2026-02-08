ddd1717447d979e6879518c7d08eebcfa109dfaf
Neomaster X <neomasterx@outlook.com>
2026-02-08 11:33:50 +0300

    #109 MainMenuGameStageBuffer

---
+++ Neomaster.PixelEarth.Presentation/Models/GameStageBuffers/MainMenuGameStageBuffer.cs
@@ -0,0 +1,9 @@
+using Neomaster.PixelEarth.App;
+
+namespace Neomaster.PixelEarth.Presentation;
+
+public sealed class MainMenuGameStageBuffer
+{
+  public TextureButton PlayButton { get; set; }
+  public TextureButton ExitButton { get; set; }
+}
