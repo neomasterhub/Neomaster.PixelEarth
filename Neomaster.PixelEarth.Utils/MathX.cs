4a53544390bd87efcc8d641e2eb664dee4e8ce80
Neomaster X <neomasterx@outlook.com>
2026-02-11 22:34:24 +0300

    #119 Remove MathX.Rectangle

--- Neomaster.PixelEarth.Utils/MathX.cs
+++ Neomaster.PixelEarth.Utils/MathX.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Numerics;
 
 namespace Neomaster.PixelEarth.Utils;
 
@@ -21,41 +20,5 @@ public static class MathX
     float gap)
   {
     return (elementCount * (elementLength + gap)) - gap;
-  }
-
-  public static class Rectangle
-  {
-    public static class Points
-    {
-      public static Vector2[] TopRight_BottomLeft(Vector4 xywh)
-      {
-        return [new(xywh.X, xywh.Y), new(xywh.X + xywh.Z, xywh.Y + xywh.W)];
-      }
-
-      public static Vector2[] TopRight_BottomLeft(float x, float y, float width, float height)
-      {
-        return [new(x, y), new(x + width, y + height)];
-      }
-    }
-
-    public static Vector2[] GetTrianglePoints_TopRight(Vector2[] topLeft_bottomRight)
-    {
-      return GetTrianglePoints_TopRight(topLeft_bottomRight[0], topLeft_bottomRight[1]);
-    }
-
-    public static Vector2[] GetTrianglePoints_BottomLeft(Vector2[] topLeft_bottomRight)
-    {
-      return GetTrianglePoints_BottomLeft(topLeft_bottomRight[0], topLeft_bottomRight[1]);
-    }
-
-    public static Vector2[] GetTrianglePoints_TopRight(Vector2 topLeft, Vector2 bottomRight)
-    {
-      return [topLeft, bottomRight, new(bottomRight.X, topLeft.Y)];
-    }
-
-    public static Vector2[] GetTrianglePoints_BottomLeft(Vector2 topLeft, Vector2 bottomRight)
-    {
-      return [topLeft, new(topLeft.X, bottomRight.Y), bottomRight];
-    }
   }
 }
