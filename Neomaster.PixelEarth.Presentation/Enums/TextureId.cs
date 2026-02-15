5cd6c43d2277184cacd1fe4fcb563a9f24779e87
Neomaster X <neomasterx@outlook.com>
2026-02-15 04:00:30 +0300

    #130 Register new main menu textures

--- Neomaster.PixelEarth.Presentation/Enums/TextureId.cs
+++ Neomaster.PixelEarth.Presentation/Enums/TextureId.cs
@@ -2,9 +2,5 @@ namespace Neomaster.PixelEarth.Presentation;
 
 public enum TextureId
 {
-  Test512x512,
-  Test512x512Hovered,
-  Test512x512Selected,
-  Test512x512SelectedHovered,
-  MainMenu,
+  MainMenuMap,
 }
