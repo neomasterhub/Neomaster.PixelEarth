fd67ddc425cbcafb5f1ee04f0038aaf2e657d68b
Neomaster X <neomasterx@outlook.com>
2026-02-22 15:08:24 +0300

    #134 IMainMenuService.HasItemWithLMBClick#get

--- Neomaster.PixelEarth.App/Services/Menu/IMainMenuService.cs
+++ Neomaster.PixelEarth.App/Services/Menu/IMainMenuService.cs
@@ -2,6 +2,7 @@ namespace Neomaster.PixelEarth.App;
 
 public interface IMainMenuService
 {
+  bool HasItemWithLMBClick { get; }
   void Initialize(MainMenu mainMenu);
   void Draw();
   void MoveUp();
