1c5b54b5400afa116faa4d0f301dce7aad6ddb25
Neomaster X <neomasterx@outlook.com>
2026-02-22 20:17:37 +0300

    #134 InitializeMainMenu() helper

--- Neomaster.PixelEarth.Presentation/Models/GameStages/LoadingGameStage.cs
+++ Neomaster.PixelEarth.Presentation/Models/GameStages/LoadingGameStage.cs
@@ -152,4 +152,113 @@ public sealed class LoadingGameStage : BaseGameStage
     _gamePipeline.RemoveGameStateFlag(GameStateFlag.Loading);
     _gamePipeline.AddGameStateFlag(GameStateFlag.ShowMainMenu);
   }
+
+  private void InitializeMainMenu(Texture map)
+  {
+    // Button
+    var w = 207;
+    var h = 50;
+    var i = 0;
+    var y = () => i++ * h;
+
+    var playButton = RectangleTextureButtonBuilder
+      .Create(texMap)
+      .Size(w, h)
+      .UvPx(0, y(), w, h)
+      .UvHoveredPx(0, y(), w, h)
+      .UvSelectedPx(0, y(), w, h)
+      .UvSelectedHoveredPx(0, y(), w, h)
+      .Build(_idGenerator.Next());
+
+    var demosButton = RectangleTextureButtonBuilder
+      .Create(texMap)
+      .Size(w, h)
+      .UvPx(0, y(), w, h)
+      .UvHoveredPx(0, y(), w, h)
+      .UvSelectedPx(0, y(), w, h)
+      .UvSelectedHoveredPx(0, y(), w, h)
+      .Action(() =>
+      {
+        _gamePipeline.RemoveGameStateFlag(GameStateFlag.ShowMainMenu);
+        _gamePipeline.AddGameStateFlag(GameStateFlag.ShowDemosMenu);
+      })
+      .Build(_idGenerator.Next());
+
+    var exitButton = RectangleTextureButtonBuilder
+      .Create(texMap)
+      .Size(w, h)
+      .UvPx(0, y(), w, h)
+      .UvHoveredPx(0, y(), w, h)
+      .UvSelectedPx(0, y(), w, h)
+      .UvSelectedHoveredPx(0, y(), w, h)
+      .Action(_gameWindowService.Exit)
+      .Build(_idGenerator.Next());
+
+    _frameService.FrameInfo.SelectedId = playButton.Id;
+
+    var mainMenu = new MainMenu
+    {
+      Items =
+      [
+        new()
+        {
+          Button = playButton,
+          DrawButton = () => _uiService.DrawRectangleTextureButton(playButton),
+        },
+        new()
+        {
+          Button = demosButton,
+          DrawButton = () => _uiService.DrawRectangleTextureButton(demosButton),
+        },
+        new()
+        {
+          Button = exitButton,
+          DrawButton = () => _uiService.DrawRectangleTextureButton(exitButton),
+        },
+      ],
+      Options = new MainMenuOptions
+      {
+        ButtonGap = 10,
+        ButtonWidth = w,
+        ButtonHeight = h,
+        HorizontalAlign = Align.Center,
+        VerticalAlign = Align.Center,
+      },
+    };
+
+    // Align items.
+    _uiService.CreateGrid(
+      mainMenu.Items.Select(x => x.Button).ToArray(),
+      PresentationConsts.WindowSettings.Width,
+      PresentationConsts.WindowSettings.Height,
+      mainMenu.Options.ButtonWidth,
+      mainMenu.Options.ButtonHeight,
+      mainMenu.Options.ButtonGap,
+      mainMenu.Options.VerticalAlign,
+      mainMenu.Options.HorizontalAlign);
+
+    _mainMenuService.Initialize(mainMenu);
+
+    // Set buffer:
+
+    // BG
+    _mainMenuGameStageBuffer.BgRectangle = new(
+      0, 0, PresentationConsts.WindowSettings.Width, PresentationConsts.WindowSettings.Height);
+    _mainMenuGameStageBuffer.BgTextureShapeOptions = new(
+      _textures.Get(TextureGroupId.MainMenu, TextureId.MainMenuBg),
+      _mainMenuGameStageBuffer.BgRectangle);
+    _mainMenuGameStageBuffer.DemosBgTextureShapeOptions = new(
+      _textures.Get(TextureGroupId.MainMenu, TextureId.MainMenuDemosBg),
+      _mainMenuGameStageBuffer.BgRectangle);
+
+    // Title
+    var tw = 742;
+    var th = 88;
+    var tx = (PresentationConsts.WindowSettings.Width - tw) / 2;
+    _mainMenuGameStageBuffer.TitleRectangle = new(
+      tx, tx, tw, th);
+    _mainMenuGameStageBuffer.TitleTextureShapeOptions = new(
+      texMap,
+      new Utils.Rectangle(w, 0, tw, th));
+  }
 }
